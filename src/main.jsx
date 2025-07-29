4d85fd52146af34b38e77c20ed5698adbc811a5b
Adan Saavedra <adansaavedraa@icloud.com>
2025-07-28 21:07:21 -0500

    cambie main para prod

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,9 @@ import { Amplify } from "aws-amplify";
 
 Amplify.configure({
   Auth: {
-    Cognito: {
-      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-      userPoolClientId: import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID,
-    },
+    region: import.meta.env.VITE_AWS_REGION,
+    userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+    userPoolClientId: import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID,
   },
   API: {
     REST: {
